72ed80b8145513b1435f21949d61b2d035d4e1bd
Lut99 <mullertim99@gmail.com>
2020-01-02 17:11:41 +0100

    Created main file for the puzzle environment

---
+++ main.py
@@ -0,0 +1,12 @@
+# MAIN.py
+#   by Lut99
+#
+# Dit bestand bevat de ingang van het programma en is dus bedoeld om uit te
+#   voeren.
+#
+# NOTE: Verander niks aan dit bestand!
+#
+
+
+def main():
+    pass
