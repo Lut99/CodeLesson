baac26a8a99b09969f718a091b3e2897080067c1
Lut99 <mullertim99@gmail.com>
2020-01-08 16:12:48 +0100

    Added ball object (or skeleton thereof)

---
+++ GameObjects/Bal.py
@@ -0,0 +1,12 @@
+# BAL.py
+#   by Lut99
+#
+# Dit bestand bevat de class voor de Bal. Het is een derived class van
+#   GameObject, en bedoeld om te worden bewogen in de puzzel.
+
+from GameBase.GameObject import GameObject
+
+
+class Ball(GameObject):
+    def __init__(self):
+        pass
