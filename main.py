3725209324c58e739916ad4717c47867d971602c
Lut99 <mullertim99@gmail.com>
2020-01-08 16:12:11 +0100

    Updated to framework skeleton

--- main.py
+++ main.py
@@ -7,6 +7,43 @@
 # NOTE: Verander niks aan dit bestand!
 #
 
+import sys
+import pygame
+import argparse
 
-def main():
-    pass
+
+def main(screen_width, screen_height):
+    print("### PUZZEL ###\n")
+
+    print("Initializeren van PyGame...", end="")
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Puzzel")
+
+    print(" Check")
+
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("\nBye.\n")
+                pygame.quit()
+                sys.exit()
+
+        # Clear the screen
+        screen.fill((pygame.white))
+
+        # Update the display
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    # Get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-W", "--width", help="De breedte (in pixels) van het scherm", type=int, default=800)
+    parser.add_argument("-H", "--height", help="De hoogte (in pixels) van het scherm", type=int, default=600)
+
+    args = parser.parse_args()
+
+    # Run main
+    main(args.width, args.height)
