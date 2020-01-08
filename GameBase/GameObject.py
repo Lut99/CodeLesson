6959647869440341e3f5580fd9a5954d28c7c567
Lut99 <mullertim99@gmail.com>
2020-01-08 16:12:38 +0100

    Added GameObject baseclass (or skeleton thereof)

---
+++ GameBase/GameObject.py
@@ -0,0 +1,12 @@
+# GAME OBJECT.py
+#   by Lut99
+#
+# De GameObject Class is afgeleid van de GameEntity class en is de basis voor
+#   alle beweegbare objecten in de puzzel.
+
+from GameBase.GameEntity import GameEntity
+
+
+class GameObject(GameEntity):
+    def __init__(self):
+        pass
