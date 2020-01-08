09c9fe33ef526288627b37f5c793642b5e71113c
Lut99 <mullertim99@gmail.com>
2020-01-08 16:44:46 +0100

    Added Ball object as test

--- main.py
+++ main.py
@@ -10,19 +10,31 @@
 import sys
 import pygame
 import argparse
+import random
+
+from GameBase.GameTime import GameTime
+from GameObjects.Ball import Ball
 
 
-def main(screen_width, screen_height):
+def main(screen_width, screen_height, framerate, updaterate):
     print("### PUZZEL ###\n")
 
     print("Initializeren van PyGame...", end="")
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Puzzel")
+    print(" Check")
 
+    print("Initializeren van framerate & update counters...", end="")
+    gtime = GameTime(framerate, updaterate)
+    print(" Check")
+
+    print(f"Level \"{'lol'}\" laden...", end="")
+    ball = Ball()
     print(" Check")
 
     # Main game loop
+    print("\nGame loop...")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,8 +42,15 @@ def main(screen_width, screen_height):
                 pygame.quit()
                 sys.exit()
 
-        # Clear the screen
-        screen.fill((pygame.white))
+        if gtime.check_framerate():
+            # Clear the screen
+            screen.fill((255, 255, 255))
+
+            # Draw object
+            ball.draw(screen)
+
+        if gtime.check_update():
+            ball.update(gtime)
 
         # Update the display
         pygame.display.update()
@@ -42,8 +61,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-W", "--width", help="De breedte (in pixels) van het scherm", type=int, default=800)
     parser.add_argument("-H", "--height", help="De hoogte (in pixels) van het scherm", type=int, default=600)
+    parser.add_argument("-f", "--framerate", help="The target number of frames per second", type=int, default=30)
+    parser.add_argument("-u", "--updaterate", help="The target number of updates per second", type=int, default=50)
 
     args = parser.parse_args()
 
     # Run main
-    main(args.width, args.height)
+    main(args.width, args.height, args.framerate, args.updaterate)
