8a92e0121e6fafd1c2b133f6535084b30dd1560e
Lut99 <mullertim99@gmail.com>
2020-01-08 16:12:57 +0100

    Added Sprite class (or skeleton thereof)

---
+++ GameBase/Sprite.py
@@ -0,0 +1,10 @@
+# SPRITE.py
+#   by Lut99
+#
+# Class die informatie opslaat over een sprite voor PyGame. Ondersteund
+#   automatisch laden van foto's en het tekenen ervan.
+
+
+class Sprite():
+    def __init__(self):
+        pass
